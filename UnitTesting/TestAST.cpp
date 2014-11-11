8cf1bbee3d21a6600113b1e6b9fe277944a79e64
Kaung Htet Zaw <emoosx@gmail.com>
2014-11-11 20:25:33 +0800

    Add testcases for pattern matching method

---
+++ UnitTesting/TestAST.cpp
@@ -0,0 +1,34 @@
+#include "AST.h"
+#include "TestAST.h"
+#include <iostream>
+
+void TestAST::setUp() {
+}
+
+void TestAST::tearDown() {
+}
+
+void TestAST::testMatchPattern() {
+	string nodePattern, queryPattern;
+
+	nodePattern = "x y + z + a + b +";
+	queryPattern = "x y +";
+	CPPUNIT_ASSERT_EQUAL(true, AST::matchPattern(nodePattern, queryPattern, 2));
+
+	queryPattern = "z a +";
+	CPPUNIT_ASSERT_EQUAL(false, AST::matchPattern(nodePattern, queryPattern, 2));
+
+	queryPattern = "a";
+	CPPUNIT_ASSERT_EQUAL(true, AST::matchPattern(nodePattern, queryPattern, 2));
+
+
+	nodePattern = "OsCar bravo + mother + father + 134 +";
+	queryPattern = "mother father +";
+	CPPUNIT_ASSERT_EQUAL(false, AST::matchPattern(nodePattern, queryPattern, 2));
+
+	queryPattern = "bravo";
+	CPPUNIT_ASSERT_EQUAL(true, AST::matchPattern(nodePattern, queryPattern, 2));
+
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestAST);

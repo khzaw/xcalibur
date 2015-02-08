34035d8b60aeb931548d753a063e6ddd1dbff35d
Kaung Htet Zaw <emoosx@gmail.com>
2015-02-09 00:33:30 +0800

    Add syntax for full grammar

--- SPA/Frontend/Parser.cpp
+++ SPA/Frontend/Parser.cpp
@@ -13,6 +13,22 @@ factor: var_name | const_value
 var_name: NAME
 proc_name: NAME
 const_value: INTEGER
+
+// full
+program: procedure+
+procedure: "procedure" proc_name "{" stmtLst "}"
+stmtLst: stmt+
+stmt: call | while | if | assign
+call: "call" proc_name ";"
+while: "while" var_name "{" stmtLst "}"
+if: "if" var_name "then" "{" stmtLst "}" "else" "{" stmtLst "}"
+assign: var_name "=" expr ";"
+expr: expr "+" term | expr "-" term | term
+term: term "*" factor | factor
+factor: var_name | const_value | "(" expr ")"
+var_name: NAME
+proc_name: NAME
+const_value: INTEGER
 */
 #include <fstream>
 #include <iostream>
